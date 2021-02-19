dfadad2a2b7981b551aee33d35fbd5dee68752b2
Abhas Tandon <abhastandon007@gmail.com>
2021-02-19 12:49:59 +0530

    Add more Indian stocks

--- utils/initial-values.ts
+++ utils/initial-values.ts
@@ -1,4 +1,8 @@
 export const INITIAL_TICKERS = [
   { name: "Reliance", ticker: "RELIANCE.BO" },
-  { name: "Indian Railway", ticker: "IRCTC.BO" },
+  { name: "Indian Railway", ticker: "IRCTC.NS" },
+  { name: "Burger King", ticker: "BURGERKING.NS" },
+  { name: "Infosys", ticker: "INFY.NS" },
+  { name: "HDFC Bank", ticker: "HDFCBANK.NS" },
+  { name: "Wipro", ticker: "WIPRO.NS" },
 ];
