5fa36131e4a9182baa917231186d1a609a3b20ec
Abhas Tandon <abhastandon007@gmail.com>
2021-02-19 12:28:10 +0530

    Change endpoint to india region

--- utils/fetch-ticker.ts
+++ utils/fetch-ticker.ts
@@ -7,7 +7,7 @@ import {
 
 export const fetchTicker = async (ticker: string): Promise<TickerResults> => {
   const { data } = await axios.get<YahooHistoryResult>(
-    `https://query2.finance.yahoo.com/v8/finance/chart/${ticker}?formatted=true&crumb=ZBpLWWzBYL9&lang=en-US&region=US&includeAdjustedClose=true&interval=1d&period1=-952374400&period2=2610841600&events=div%7Csplit&useYfid=true`
+    `https://query2.finance.yahoo.com/v8/finance/chart/${ticker}?formatted=true&crumb=ZBpLWWzBYL9&lang=en-IN&region=IN&includeAdjustedClose=true&interval=1d&period1=-952374400&period2=2610841600&events=div%7Csplit&useYfid=true`
   );
 
   const result = data.chart.result?.[0];
@@ -38,7 +38,7 @@ export const fetchTickerProfile = async (
   ticker: string
 ): Promise<TickerProfile> => {
   const { data } = await axios.get<YahooProfileResult>(
-    `https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&crumb=ZBpLWWzBYL9&lang=en-US&region=US&symbols=${ticker}&fields=shortName%2CmarketCap%2CunderlyingExchangeSymbol`
+    `https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&crumb=ZBpLWWzBYL9&lang=en-IN&region=IN&symbols=${ticker}&fields=shortName%2CmarketCap%2CunderlyingExchangeSymbol`
   );
 
   const result = data.quoteResponse?.result?.[0];
